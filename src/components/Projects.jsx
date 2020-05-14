ac79d4c5437342dbddb2a568ca0b247f14db31ca
Joseph Kim <josephkim570@gmail.com>
2020-05-14 19:35:23 -0400

    Ended up using styled-components and I really like it. Project component has cards housing placeholder title and text and an empty href that will house the link to project repo

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,4 +1,32 @@
 import React, {Component} from 'react';
+import styled from 'styled-components';
+
+const Card = styled.div`
+    padding: 1rem;
+    margin: 1rem;
+    background: papayawhip;
+    border: 1px solid black;
+`;
+
+const Wrapper = styled.section`
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+`;
+
+const CardWrapper = styled.div`
+    border: 1px solid black;
+    margin: 0.5rem;
+`;
+
+const Project = styled.div`
+    display: flex;
+    justify-content: center;
+    padding: auto 0.5rem;
+    border: 1px solid black;
+    border-radius: 5px;
+    margin: 1rem;
+`;
 
 class Projects extends Component {
     constructor(props) {
@@ -8,7 +36,53 @@ class Projects extends Component {
     render() { 
         return (  
             <div>
-                <h2>Projects</h2>
+                <Project><h2>Projects</h2></Project>
+                <Wrapper>
+                    <Card>
+                        <h4><a href="#">Project 1</a></h4>
+                        <CardWrapper>
+                            <h5>Project Description:</h5>
+                            <p>placeholder jabber text text text text text</p>
+                            <h5>Primary Responsibilities:</h5>
+                            <p>placeholder jibber jabbber text more text</p>
+                            <h5>Tech Stack:</h5>
+                            <p>placeholder jibber jabber woopie text text text asdasdasd</p>
+                        </CardWrapper>
+                    </Card>
+                    <Card>
+                        <h4><a href="#">Project 2</a></h4>
+                        <CardWrapper>
+                            <h5>Project Description:</h5>
+                            <p>placeholder jabber text text text text text</p>
+                            <h5>Primary Responsibilities:</h5>
+                            <p>placeholder jibber jabbber text more text</p>
+                            <h5>Tech Stack:</h5>
+                            <p>placeholder jibber jabber woopie text text text asdasdasd</p>
+                        </CardWrapper>
+                    </Card>
+                    <Card>
+                        <h4><a href="#">Project 3</a></h4>
+                        <CardWrapper>
+                            <h5>Project Description:</h5>
+                            <p>placeholder jabber text text text text text</p>
+                            <h5>Primary Responsibilities:</h5>
+                            <p>placeholder jibber jabbber text more text</p>
+                            <h5>Tech Stack:</h5>
+                            <p>placeholder jibber jabber woopie text text text asdasdasd</p>
+                        </CardWrapper>
+                    </Card>
+                    <Card>
+                        <h4><a href="#">Project 4</a></h4>
+                        <CardWrapper>
+                            <h5>Project Description:</h5>
+                            <p>placeholder jabber text text text text text</p>
+                            <h5>Primary Responsibilities:</h5>
+                            <p>placeholder jibber jabbber text more text</p>
+                            <h5>Tech Stack:</h5>
+                            <p>placeholder jibber jabber woopie text text text asdasdasd</p>
+                        </CardWrapper>
+                    </Card>
+                </Wrapper>
             </div>
         );
     }
