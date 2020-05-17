ad74df08012a41e73931c91259f4273c275bae0f
Joseph Kim <josephkim570@gmail.com>
2020-05-16 20:31:36 -0400

    modified setup reverting from bootstrap to styled-components, turns out the key to responsive issues was to change width to max width, and placing image into the wrapper + background and centering everything

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,17 +1,23 @@
 import React, { Component } from 'react';
 import CV from '../cv/Joseph_Kim_Software_Dev.pdf';
 import styled from 'styled-components';
-import { Card, CardDeck } from 'react-bootstrap';
+import { Card, CardDeck, ListGroup } from 'react-bootstrap';
 import profilePic from '../images/rightPic2.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFileDownload } from '@fortawesome/free-solid-svg-icons';
 
 const NameTitle = styled.h1`
     font-weight: bold;
-    
+    display: flex;
+    justify-content: center;
 `;
 
 const JobTitle = styled.h4`
     font-weight: 300;
     font-style: italic;
+    margin-bottom: 2rem;
+    display: flex;
+    justify-content: center;
 `;
 
 const LandingCardBackground = styled.div`
@@ -24,7 +30,8 @@ const LandingCardBackground = styled.div`
 const EmptyLandingCardBackground = styled.div`
     padding: 1rem;
     margin: 1rem;
-    border: 1px solid black;
+    display: flex;
+    justify-content: center;
 `;
 
 const LandingWrapper = styled.section`
@@ -42,104 +49,57 @@ class Portfolio extends Component {
     render() { 
         return (  
             <div>
-                <CardDeck>
-  <Card>
-    <Card.Body>
-      <Card.Title>
-          <NameTitle>I'm Joseph Kim</NameTitle>
-          <JobTitle>Software Developer</JobTitle>
-      </Card.Title>
-      <Card.Text>
-      I have always enjoyed the idea of building something from nothing. 
-     With each step, one can see the progress from a mere thought to the deployment stage. 
-    The little pieces that tell a story from the many failures and successes resulting from the countless hours put forth.     
-      </Card.Text>
-    <Card.Text>
-    I take great pride in the work that I am able to achieve and contribute to my team. 
-                                I have had great opportunities working with like-minded individuals, 
-                                which help to reinforce my teamwork capabilties and desire for constant learning.
-    </Card.Text>
-
-    <Card.Text style={{fontStyle:"italic"}}>
-        Customer service is the cornerstone of any business.
-    </Card.Text>
-
-    <Card.Text>      
-        That being said, I believe that together with my experience in programming and in client management, 
-        I am a valuable asset that is equipped with the ability to colloborate efficiently amongst internal and external stakeholders.
-    </Card.Text>
-    
-    <Card.Text style={{fontWeight:'bold'}}>
-        If interested, please email me at josephkim570@gmail.com
-    </Card.Text>
-
-    <Card.Text>
-        <a href={CV} target='_blank' rel="noopener noreferrer"><button>Download CV (ADD ICON HERE)</button></a>
-    </Card.Text>
-
-    </Card.Body>
-  </Card>
-  <Card>
-    <Card.Img variant="top" src={profilePic} />
-
-  </Card>
-</CardDeck>
-                <LandingWrapper>
-                <LandingCardBackground>
-                <Card style={{ width: '30rem' }}>
-                        <Card.Body>
-                        <NameTitle>I'm Joseph Kim</NameTitle>
-                <JobTitle>Software Developer</JobTitle>
-                            <Card.Text>
-                            <p>
-                                I have always enjoyed the idea of building something from nothing. 
-                                With each step, one can see the progress from a mere thought to the deployment stage. 
-                                The little pieces that tell a story from the many failures and successes resulting from the countless hours put forth.
-                            </p>
-                            <p>
-                                I take great pride in the work that I am able to achieve and contribute to my team. 
-                                I have had great opportunities working with like-minded individuals, 
-                                which help to reinforce my teamwork capabilties and desire for constant learning.
-                            </p>
-                            <p style={{fontStyle: 'italic'}}>
-                            Customer service is the cornerstone of any business.
-                        </p>
-                        <p>
-                            That being said, I believe that together with my experience in programming and in client management, 
-                            I am a valuable asset that is equipped with the ability to colloborate efficiently amongst internal and external stakeholders.
-                        </p>
-                        <p>
-                            Allow me to showcase a small portion of my experiences and future potential below.
-                        </p>
-                        <p style={{fontWeight: 'bold'}}>
-                            If interested, please email me at josephkim570@gmail.com
-                        </p>
-                        <p>
-                        <a href={CV} target='_blank' rel="noopener noreferrer"><button>Download CV (ADD ICON HERE)</button></a>
-                        </p>
-                        </Card.Text>
-                        </Card.Body>
-                        </Card>
+                <ListGroup>
+                    <ListGroup.Item>Cras justo odio</ListGroup.Item>
+                    <ListGroup.Item>Dapibus ac facilisis in</ListGroup.Item>
+                    <ListGroup.Item>Morbi leo risus</ListGroup.Item>
+                    <ListGroup.Item>Porta ac consectetur ac</ListGroup.Item>
+                    <ListGroup.Item>Vestibulum at eros</ListGroup.Item>
+                </ListGroup>
+                <div>
+                    <LandingWrapper> 
+                        <LandingCardBackground>
+                            <Card style={{ maxWidth: '50rem' }}>
+                                <Card.Body>
+                                    <EmptyLandingCardBackground>
+                                        <img src={ profilePic } alt='profile'style={{boxShadow: '3px 3px 3px 4px gray'}}></img>
+                                    </EmptyLandingCardBackground>
+                                    <NameTitle>I'm Joseph Kim</NameTitle>
+                                    <JobTitle>Software Developer</JobTitle>
+                                    <Card.Text>
+                                        I have always enjoyed the idea of building something from nothing. 
+                                        With each step, one can see the progress from a mere thought to the deployment stage. 
+                                        The little pieces that tell a story from the many failures and successes resulting from the countless hours put forth.
+                                    </Card.Text>
+                                    <Card.Text>
+                                        I take great pride in the work that I am able to achieve and contribute to my team. 
+                                        I have had great opportunities working with like-minded individuals, 
+                                        which help to reinforce my teamwork capabilties and desire for constant learning.
+                                    </Card.Text>
+                                    <Card.Text
+                                        style={{fontStyle: 'italic'}}>
+                                        Customer service is the cornerstone of any business.
+                                    </Card.Text>
+                                    <Card.Text>
+                                        That being said, I believe that together with my experience in programming and in client management, 
+                                        I am a valuable asset that is equipped with the ability to colloborate efficiently amongst internal and external stakeholders.
+                                    </Card.Text>
+                                    <Card.Text>
+                                        Allow me to showcase a small portion of my experiences and future potential below.
+                                    </Card.Text>
+                                    <Card.Text style={{fontWeight: 'bold'}}>
+                                        If interested, please email me at josephkim570@gmail.com
+                                    </Card.Text>
+                                    <Card.Text>
+                                        <a href={CV} target='_blank' rel="noopener noreferrer"><button>Download CV <FontAwesomeIcon icon={faFileDownload}/></button></a>
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
                         </LandingCardBackground>
-                
-             
-                <EmptyLandingCardBackground>
-                <Card style={{ width: '30rem' }}>
-                        <Card.Body>
-                            <Card.Title>Title</Card.Title>
-                            <Card.Text>
-                            <img src={ profilePic }></img>
-                        </Card.Text>
-                        </Card.Body>
-                        </Card>
-                 </EmptyLandingCardBackground>
-                 
-                        </LandingWrapper>
-                    
+                    </LandingWrapper>
+                </div>
             </div>
-
         );
     }
 }
- 
 export default Portfolio;
